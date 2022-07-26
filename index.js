25f6f44faaef379f6005aac613a71341dde3f969
Udiesha gautam <87614825+Udiesha@users.noreply.github.com>
2022-07-26 21:51:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+ const squareUtil = require("./squareUtil");
+
+
+/* destructuring
+const {data, dataNew, multiple} = require("./squareUtil");
+console.log(data);
+console.log(dataNew);
+console.log(multiple(2,3))*/
+
+ console.log(squareUtil.multiple(2,3));
+
+// let data = squareUtil.squareFn(2);
+// console.log(data);
+// let addi=squareUtil.add(2,3);
+// console.log(addi);
+
+
+// let companyName = "habilelabs";
+
+// let isFirstCompany = () => {
+//     return false;
+// }
+
+//  const data1 = {
+//     name : "tanvi",
+
+//      company : companyName,
+//     // pahliCompany : isFirstCompany
+//     pahliCompany : isFirstCompany()
+    
+//  }
+
+
+//  console.log(data1);
---
+++ squareUtil.js
@@ -0,0 +1,18 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const multiple =  (num1, num2) => {
+   return num1 * num2;
+}
+
+module.exports = {
+   squareFn : (number) => {
+       return number * number;
+   },    
+   data : "test",
+   dataNew : true,
+   add,
+   multiple
+
+} 
